3b3d454445b31facfbd0935cde0fa70b25d8caca
wuqinqin778 <1009697209@qq.com>
2020-12-18 19:34:31 +0800

    删除

--- src/http/api.js
+++
@@ -1,6 +0,0 @@
-import http from './index'
-//const base = process.env.BASE_URL !== '/' ? process.env.BASE_URL : '/api';
-export function getRoute () {
-  // return http.post(`${base}/getVehicleList`)
-  return http.post('http://server.leoking.com/api/gps/getVehicleList')
-}
